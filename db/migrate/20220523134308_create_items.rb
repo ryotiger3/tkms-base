4184cbc9a9903e528fa1c74e44a66fae3a7d67a3
ryotiger3 <rothsryo319@gmail.com>
2022-05-23 22:44:10 +0900

    itemモデル作成

---
+++ db/migrate/20220523134308_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string     :name,                null: false
+      t.integer    :price,               null: false
+      t.text       :description,         null: false
+      t.integer    :category_id,         null: false
+      t.integer    :shipping_days_id,    null: false
+      t.references :user,                foreign_key: true
+
+      t.timestamps
+    end
+  end
