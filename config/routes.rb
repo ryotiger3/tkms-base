059235ec8f5a3485b9f9dd13dc6443c53ec9a27f
ryotiger3 <rothsryo319@gmail.com>
2022-05-24 17:11:06 +0900

    修正・追加-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
   devise_for :users
     root to: "items#index"
     resources :items do
